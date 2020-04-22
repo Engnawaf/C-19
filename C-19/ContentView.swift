1477f0d1257bfae2a2ab34585c39e86b8e20a3fd
Nawaf <eng_nwf@live.com>
2020-04-22 18:44:20 +0300

    Initial Commit

---
+++ C-19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  C-19
+//
+//  Created by Nawaf Al-Failakawy on 4/22/20.
+//  Copyright © 2020 Nawaf Al-Failakawy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
